19ff294b8c84b85d3de19d197bcd7374b1e16ff5
Graham Nygard <nygard@macaroni-04.cs.wisc.edu>
2015-03-16 20:44:36 -0500

    Slab and Next Fit free functions written. The coalescing code is purely within the Next Fit code, and this assumes that slab blocks were originally partitioned

--- p3/nygard/mymem.h
+++ p3/nygard/mymem.h
@@ -3,6 +3,8 @@
 
 void * Mem_Init(int sizeOfRegion, int slabSize);
 void * Mem_Alloc(int size);
+static void * slab_free(void * ptr);
+static void * nf_free(void * ptr);
 int Mem_Free(void *ptr);
 void Mem_Dump();
 
